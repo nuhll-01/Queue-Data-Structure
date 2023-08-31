19f68690c7cd37fd7bf35679351a49b0e188a25a
ToastyyX <erick.cool321@icloud.com>
2023-08-30 23:07:57 -0500

    Additional Functionality + Bug Fixes

--- src/Queue.java
+++ src/Queue.java
@@ -1,5 +1,6 @@
 import java.util.NoSuchElementException;
-public class Queue  {
+
+public class Queue implements Cloneable {
 
     private float[] data; // The array that holds the items.
     private int manyItems; // The number of elements in the queue.
@@ -9,7 +10,7 @@ public class Queue  {
     public Queue() {
         final int INITIAL_CAPACITY = 10;
         manyItems = 0;
-        data =  new float[INITIAL_CAPACITY];
+        data = new float[INITIAL_CAPACITY];
     }
 
     public Queue(int initialCapacity) {
@@ -21,9 +22,9 @@ public class Queue  {
     }
 
     public void add(float item) throws OutOfMemoryError {
-        // if (manyItems == data.length) {
-        //     ensureCapacity((manyItems * 2) + 1);
-        // }
+        if (manyItems == data.length) {
+            ensureCapacity((manyItems * 2) + 1);
+        }
         if (manyItems == 0) {
             front = 0;
             rear = 0;
@@ -38,38 +39,87 @@ public class Queue  {
         if (i == data.length - 1) {
             return 0;
         } else {
-            return i + 1;
+            return ++i;
         }
     }
 
     public float remove() throws NoSuchElementException {
+        float answer;
+
         if (manyItems == 0) {
-            throw new NoSuchElementException("Queue is empty.");
+            throw new NoSuchElementException("Queue Underflow.");
         }
-        float answer  = data[front];
-        data[front++] = 0;
+        answer = data[front];
+        data[front] = 0;
+        front = nextIndex(front);
+        manyItems--;
         return answer;
     }
 
-    public void print() {
-        for (int i = 0; i < manyItems; i++) {
-            System.out.println(data[i]);
+    public void ensureCapacity(int minimumCapacity) throws OutOfMemoryError {
+        float[] biggerArray;
+        int n1, n2;
+        if (data.length >= minimumCapacity) {
+        } else if (manyItems == 0) {
+            data = new float[minimumCapacity];
+        } else if (manyItems > 0 && front <= rear) {
+            biggerArray = new float[minimumCapacity];
+            System.arraycopy(data, front, biggerArray, front, manyItems);
+            data = biggerArray;
+        } else {
+            biggerArray = new float[minimumCapacity];
+            n1 = data.length - front;
+            n2 = rear + 1; //
+            System.arraycopy(data, front, biggerArray, 0, n1);
+            System.arraycopy(data, 0, biggerArray, n1, n2);
+            front = 0;
+            rear = manyItems - 1;
+            data = biggerArray;
         }
     }
 
-    // public void ensureCapacity(int minimumCapacity) {
-    //     float[] biggerArray;
-//
-    //     int intValue = manyItems;
-    //     if (data.length < minimumCapacity) {
-    //         biggerArray = new float[minimumCapacity];
-    //         System.arraycopy(data, 0, biggerArray, 0, intValue);
-    //         data = biggerArray;
-    //     }
-    // }
+    public int indexOf(float element) {
+        int answer = -1;
+        int i = 0;
+        while (i < manyItems && answer == -1) {
+            if (element == data[i]) {
+                answer = i;
+            }
+            i++;
+        }
+        return answer;
+    }
+
+    @Override
+    public Queue clone() {
+        Queue clone;
+        try {
+            clone = (Queue) super.clone();
+        } catch (CloneNotSupportedException exception) {
+            throw new RuntimeException("No 'Cloneable' interface found.");
+        }
+        clone.data = data.clone();
+        return clone;
+    }
+
+    // Write a method that prints the contents of the queue.
+
+    public void print() {
+        for (int i = 0; i < data.length; ++i) {
+            System.out.println(data[i] + " ");
+        }
+    }
 
     public boolean isEmpty() {
         return manyItems == 0;
+    }
+
+    public float getRear() {
+        return data[rear];
+    }
+
+    public float getFront() throws ArrayIndexOutOfBoundsException {
+        return data[front];
     }
 
     public int getCapacity() {
