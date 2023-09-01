fecb3feb0823c758c91c03bf95e1c30ce0cf10cb
ToastyyX <erick.cool321@icloud.com>
2023-08-31 21:47:14 -0500

    Fixed Bug on 'indexOf' Method + Additional Functionality

--- src/Queue.java
+++ src/Queue.java
@@ -1,8 +1,9 @@
+import javax.swing.plaf.LabelUI;
 import java.util.NoSuchElementException;
 
 public class Queue implements Cloneable {
 
-    private float[] data; // The array that holds the items.
+    private int[] data; // The array that holds the items.
     private int manyItems; // The number of elements in the queue.
     private int front; // The index of the front item.
     private int rear; // The index of the rear item
@@ -10,7 +11,7 @@ public class Queue implements Cloneable {
     public Queue() {
         final int INITIAL_CAPACITY = 10;
         manyItems = 0;
-        data = new float[INITIAL_CAPACITY];
+        data = new int[INITIAL_CAPACITY];
     }
 
     public Queue(int initialCapacity) {
@@ -18,10 +19,10 @@ public class Queue implements Cloneable {
             throw new IllegalArgumentException("Invalid Capacity. " + "\nYou entered: " + initialCapacity);
         }
         manyItems = 0;
-        data = new float[initialCapacity];
+        data = new int[initialCapacity];
     }
 
-    public void add(float item) throws OutOfMemoryError {
+    public void add(int item) throws OutOfMemoryError {
         if (manyItems == data.length) {
             ensureCapacity((manyItems * 2) + 1);
         }
@@ -35,16 +36,8 @@ public class Queue implements Cloneable {
         manyItems++; // Indicates that there's now an element in the array.
     }
 
-    private int nextIndex(int i) {
-        if (i == data.length - 1) {
-            return 0;
-        } else {
-            return ++i;
-        }
-    }
-
-    public float remove() throws NoSuchElementException {
-        float answer;
+    public int remove() throws NoSuchElementException {
+        int answer;
 
         if (manyItems == 0) {
             throw new NoSuchElementException("Queue Underflow.");
@@ -56,20 +49,28 @@ public class Queue implements Cloneable {
         return answer;
     }
 
+    private int nextIndex(int i) {
+        if (i == data.length - 1) {
+            return 0;
+        } else {
+            return ++i;
+        }
+    }
+
     public void ensureCapacity(int minimumCapacity) throws OutOfMemoryError {
-        float[] biggerArray;
+        int[] biggerArray;
         int n1, n2;
         if (data.length >= minimumCapacity) {
         } else if (manyItems == 0) {
-            data = new float[minimumCapacity];
+            data = new int[minimumCapacity];
         } else if (manyItems > 0 && front <= rear) {
-            biggerArray = new float[minimumCapacity];
+            biggerArray = new int[minimumCapacity];
             System.arraycopy(data, front, biggerArray, front, manyItems);
             data = biggerArray;
         } else {
-            biggerArray = new float[minimumCapacity];
+            biggerArray = new int[minimumCapacity];
             n1 = data.length - front;
-            n2 = rear + 1; //
+            n2 = rear + 1;
             System.arraycopy(data, front, biggerArray, 0, n1);
             System.arraycopy(data, 0, biggerArray, n1, n2);
             front = 0;
@@ -78,10 +79,10 @@ public class Queue implements Cloneable {
         }
     }
 
-    public int indexOf(float element) {
+    public int indexOf(int element) {
         int answer = -1;
         int i = 0;
-        while (i < manyItems && answer == -1) {
+        while (i <= manyItems && answer == -1) {
             if (element == data[i]) {
                 answer = i;
             }
@@ -102,7 +103,32 @@ public class Queue implements Cloneable {
         return clone;
     }
 
-    // Write a method that prints the contents of the queue.
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < manyItems; i++) {
+            sb.append(data[(front + i) % data.length]);
+            if (i < manyItems - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public void clear() {
+        int i;
+        for (i = 0; i < data.length; i++) {
+            data[i] = 0;
+        }
+        front = 0;
+        rear = 0;
+        manyItems = 0;
+    }
+
+    public boolean contains(int element) {
+        return indexOf(element) != -1;
+    }
 
     public void print() {
         for (int i = 0; i < data.length; ++i) {
@@ -114,12 +140,12 @@ public class Queue implements Cloneable {
         return manyItems == 0;
     }
 
-    public float getRear() {
-        return data[rear];
+    public int getFront() throws ArrayIndexOutOfBoundsException {
+        return data[front];
     }
 
-    public float getFront() throws ArrayIndexOutOfBoundsException {
-        return data[front];
+    public int getRear() {
+        return data[rear];
     }
 
     public int getCapacity() {
